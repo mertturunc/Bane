36147565f01c8234455ea5c49dfdc87e137f691b
Ilteris EROGLU <linuxgemini@users.noreply.github.com>
2016-04-23 19:04:58 +0300

    added motd to config

--- Bane.js
+++ Bane.js
@@ -43,4 +43,4 @@ bot.on("message", function (message) {
 });
 
 bot.loginWithToken(config.token);
-setTimeout(function(){bot.setStatus("online", "*yardım");}, 2000);
+setTimeout(function(){bot.setStatus("online", config.startupGame);}, 2000);
