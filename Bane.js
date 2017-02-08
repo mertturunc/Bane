b7c29c96b40dbac7fa024e22410eb1c156b32fef
Ilteris EROGLU <linuxgemini@users.noreply.github.com>
2017-02-08 20:25:15 +0200

    Add files via upload

--- Bane.js
+++ Bane.js
@@ -11,8 +11,7 @@ try {
     fs.writeFileSync(jsonFolder + 'config.json', fs.readFileSync(jsonFolder + 'config-base.json'));
 }
 
-var config = require(jsonFolder + 'config.json');
-const trigger = config.trigger;
+const config = require(jsonFolder + 'config.json');
 
 let commands = require('./Komutlar.js').commands;
 let aliases = require(jsonFolder + "Aliases.json");
@@ -24,7 +23,7 @@ bot.on("ready", () => {
     console.log("Başladı! Şu an " + bot.guilds.size + " adet sunucuya (ve " + bot.channels.size + " kanala) hizmet veriyorum.");
     setTimeout(function() {
         bot.user.setStatus("online");
-		setTimeout(function(){bot.user.setGame(config.startupGame);}, 833);
+        setTimeout(function(){bot.user.setGame(config.startupGame);}, 833);
     }, 2000)
 });
 
@@ -33,21 +32,35 @@ bot.on("error", (error) => {
 });
 
 bot.on("message", message => {
-    let msg = message.content;
-    if (msg[0] === trigger) {
-        let command = msg.toLowerCase().split(" ")[0].substring(1);
-        let suffix = msg.substring(command.length + 2);
-        if (commands[command]) {
-            commands[command].process(bot, message, suffix);
-        } else {
-            if (aliases[command]) {
-                var aliasEd = aliases[command];
-                commands[aliasEd].process(bot, message, suffix);
-            } else {}
-        };
+    if (message.author.bot === true) {
+        return;
+    };
+
+    if (!message.content.startsWith(config.trigger)) {
+        return;
+    };
+
+    let command = message.content.split(" ")[0];
+    command = command.slice(config.trigger.length);
+
+    let suffix = message.content.substring(command.length + (config.trigger.length + 1));
+
+    let args = message.content.split(" ").slice(1);
+
+    if (commands[command]) {
+        commands[command].process(bot, message, suffix, args);
     } else {
-    	return;
-    }
+        if (aliases[command]) {
+            var aliasEd = aliases[command];
+            commands[aliasEd].process(bot, message, suffix, args);
+        } else {
+            return;
+        };
+    };
+});
+
+bot.on("guildCreate", guild => {
+    console.log(`Yeni bir sunucuya katıldım, adı $(guild) ve sahibi $(guild.owner.user.username)`)
 });
 
 bot.login(config.token);
